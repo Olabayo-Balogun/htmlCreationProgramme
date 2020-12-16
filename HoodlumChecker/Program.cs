2b5f27d498d1ab077a7d86a7f705297465846e4b
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2020-12-16 12:50:05 +0100

    Add project files.

---
+++ HoodlumChecker/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Timers;
+
+public class Example
+{
+    private static Timer aTimer;
+    private static List<String> eventlog;
+    private static int nEventsFired = 0;
+    private static DateTime previousTime;
+
+    public static void Main()
+    {
+        eventlog = new List<String>();
+
+        StreamWriter sr = new StreamWriter("C:\\users\\OlabayoBalogun\\source\\repos\\htmlCreationProgramme\\htmlCreationProgramme\\hoodlumChecker.html");
+
+        // Create a timer with a five millisecond interval.
+        aTimer = new Timer(5000);
+       
+        aTimer.Elapsed += OnTimedEvent;
+
+        // Hook up the Elapsed event for the timer. 
+        aTimer.AutoReset = true;
+        sr.WriteLine("The timer should fire every {0} milliseconds. <br>",
+                     aTimer.Interval);
+        aTimer.Enabled = true;
+
+        Console.WriteLine("Press the Enter key to exit the program... ");
+        Console.ReadLine();
+        foreach (var item in eventlog)
+            sr.WriteLine(item);
+        sr.Close();
+        Console.WriteLine("Terminating the application...");
+    }
+
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        string time = DateTime.Now.ToString("G");
+        
+        eventlog.Add(String.Format($"The time is {time} <q>No hoodlum on site</q> <br>",
+                                   e.SignalTime,
+                                   nEventsFired++ == 0 ?
+                                      0.0 : (e.SignalTime - previousTime).TotalMilliseconds));
+        previousTime = e.SignalTime;
+        if (nEventsFired == 20)
+        {
+            Console.WriteLine("No more events will fire... ");
+            aTimer.Enabled = false;
+        }
+    }
+}
---
+++ htmlCreationProgramme/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace htmlCreationProgramme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring a username
+            Console.WriteLine("Please enter a username");
+            string userName = Console.ReadLine();
+            userName = $"<span style='color:red; font-size: 10vw;'>Your username is: </span>{userName.ToString()}";
+            userName = $"<header style='background-color: blue; font-size: 9vw;' >{userName}</header>";
+
+            //Declaring a file name
+            Console.WriteLine("Please enter a file name");
+            string fileName = Console.ReadLine();
+            fileName = fileName.ToString();
+
+            //Declaring the filepath
+            string filePath = $"C:\\users\\OlabayoBalogun\\source\\repos\\htmlCreationProgramme\\htmlCreationProgramme\\{fileName}.html";
+
+
+            //Declaring the blog content
+            Console.WriteLine("Please write your blog's content");
+            string blogContent = Console.ReadLine();
+            blogContent = $"<p style='background-color: green; font-size: 5vw;'>{blogContent.ToString()}</p>";
+
+            Console.WriteLine("Please enter the date published");
+            string publishDate = $"<footer style='background-color: yellow; font-size: 5vw;'> { DateTime.Now.ToString("G")}</footer>";
+
+
+            string fileContent = $"{userName} <br> {blogContent} <br> {publishDate}";
+
+            //Creating the HTML file.
+            File.WriteAllText(filePath, $"{fileContent}");
+
+
+            
+            Console.WriteLine("HTML File created.");
+            Console.ReadLine();
+        }
+    }
+}
